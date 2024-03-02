a644f22e1d72d47c30fb8797d231a6f2cf4b088a
Wylhem <wylhemdorv@gmail.com>
2024-03-02 18:35:33 +0100

    change

--- src/app/components/template/templating.tsx
+++ src/app/components/template/templating.tsx
@@ -4,7 +4,7 @@ export  function  Templating() {
         <div className={"m-3 text-[#103362] text-xl"}>
           Je voulais te souhaiter un joyeux anniversaire, Le meilleur que tu puisses avoir <br/>
           Je suis vraiment pas très doué parfois pour parler je réfléchis trop et je me dis que je vais dire une connerie <br/>
-          mais je voulais te dire que je name et que je suis tellement heureux  de t&apos;avoir dans ma vie <br/>
+          mais je voulais te dire que je t&apos;aime et que je suis tellement heureux  de t&apos;avoir dans ma vie <br/>
           sans toi je sais que ma vie serait tellement différente (en moins bien) <br/>
           Tu es un personne incroyable et j&apos;aimerais que tu ne l&apos;oublie jamais <br/>
 
